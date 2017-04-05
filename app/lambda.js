c2ecde971474d547248beaccfb659d2fff321f72
Paweł Barszcz <pawelbarszcz@gmail.com>
2017-04-05 22:56:12 +0200

    '/version' endpoint with version of deployed Lambda / API Gateway

--- app/lambda.js
+++ app/lambda.js
@@ -11,3 +11,7 @@ api.get('/greet', function (request) {
     var superb = require('superb');
     return request.queryString.name + ' is ' + superb();
 });
+
+api.get('/version', function (request) {
+    return request.context.stage;
+});
