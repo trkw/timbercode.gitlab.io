6476b68fe241f36e1403cd6e7faaa0a24f9eadaf
Paweł Barszcz <pawelbarszcz@gmail.com>
2017-04-28 22:03:58 +0200

    Sample post with embedded slides

---
+++ timbercode-website/pages/blog/POSTS/dsp17-prezentacja-o-migracji-bloga.vue
@@ -0,0 +1,16 @@
+<template>
+    <Post :post="post"></Post>
+</template>
+
+<script>
+  import post from './dsp17-prezentacja-o-migracji-bloga.md'
+  import Post from '~components/Post.vue'
+  export default {
+    asyncData () {
+      return {
+        post
+      }
+    },
+    components: {Post}
+  }
+</script>
